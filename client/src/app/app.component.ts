7e2671602e6bc2637969e9cd9a14fceae03a30ad
gnulitskaya <webgnule@mail.ru>
2024-09-05 15:37:00 +0300

    get products

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +14,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/';
+  products: Product[] = [];
   private http = inject(HttpClient);
   
   ngOnInit(): void {
-    this.http.get(this.baseUrl + 'products').subscribe(data => {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe((data) => {
       console.log(data);
+      this.products = data.data;
     })
   }
 }
