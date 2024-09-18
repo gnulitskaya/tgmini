eef1c7f3922c76278367b93e5c059abac064b408
gnulitskaya <webgnule@mail.ru>
2024-09-18 14:50:45 +0300

    add itemsCount

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Cart, CartItem } from '../../shared/models/cart';
@@ -12,6 +12,9 @@ export class CartService {
   baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
   cart = signal<Cart | null>(null);
+  itemsCount = computed(() => {
+    return this.cart()?.items.reduce((acc, item) => acc + item.quantity, 0);
+  })
   constructor() { }
 
   getCart(id: string) {
